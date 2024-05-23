b28f33a93b42b91d65afc8ba833af5e80b124eb2
CreedJA <Jordan.H.Creed@moffitt.org>
2024-05-23 17:28:11 -0400

    lecture 1 notes

---
+++ notes/lecture_01.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(here)
+
+# r works as a calculator
+7*8*3-2
+
+# <- assignment
+
+object_one <- 3*4
+
+this_is_a_really_really_really_long_name <- 8
+This_is_a_really_really_really_long_name
+
+object_two <- c(86, 11, 52, 8)
+object_two*2
+
+object_three <- c(object_two, 67)
+object_three <- c(object_two, "six")
+
+sum(object_two)
+
+object_four <- object_one + 7
+object_one <- 6
